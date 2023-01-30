0976f89d2f6879a7ef658fc4ca12aa1ed2e245c0
Curetix <me@curetix.eu>
2023-01-30 23:27:58 +0100

    Use dark theme by default

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,16 +1,29 @@
-import { MantineProvider } from "@mantine/core";
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from "@mantine/core";
 import type { EmotionCache } from "@mantine/core";
+import { useColorScheme } from "@mantine/hooks";
 import { NotificationsProvider } from "@mantine/notifications";
 import type { PropsWithChildren } from "react";
+import { useState } from "react";
 
 interface Props extends PropsWithChildren {
   emotionCache?: EmotionCache;
 }
 
 export function ThemeProvider({ emotionCache, children }: Props) {
+  const preferredColorScheme = useColorScheme("dark");
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS emotionCache={emotionCache}>
-      <NotificationsProvider>{children}</NotificationsProvider>
-    </MantineProvider>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+        emotionCache={emotionCache}>
+        <NotificationsProvider>{children}</NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
