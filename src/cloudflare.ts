d373e044e2ce87e0d6b319fabc1e0872f137be46
Curetix <me@curetix.eu>
2023-01-30 14:39:28 +0100

    Add Cloudflare API types

---
+++ src/cloudflare.ts
@@ -0,0 +1,89 @@
+const CloudflareApiBaseUrl = "https://api.cloudflare.com/client/v4";
+
+type CloudflareVerifyToken = {
+  expires_on?: string;
+  id: string;
+  not_before?: string;
+  status: string;
+};
+
+type CloudflareZone = {
+  id: string;
+  name: string;
+  status: string;
+};
+
+type CloudflareEmailDestination = {
+  created: string;
+  email: string;
+  modified: string;
+  tag: string;
+  verified: string;
+};
+
+type CloudflareEmailRule = {
+  actions: {
+    type: "forward" | "worker";
+    value: string[];
+  }[];
+  matchers: {
+    field: "to";
+    type: "literal";
+    value: string;
+  }[];
+  enabled: boolean;
+  name: string;
+  priority: number;
+  tag?: string;
+};
+
+type CloudflareResponseResultInfo = {
+  count: number;
+  page: number;
+  per_page: number;
+  total_count: number;
+};
+
+type CloudflareSuccessResponse<T> = {
+  errors: [];
+  messages: {
+    code: number;
+    message: string;
+  }[];
+  result: T;
+  success: true;
+  result_info: CloudflareResponseResultInfo;
+};
+
+type CloudflareErrorResponse = {
+  errors: {
+    code: number;
+    message: string;
+  }[];
+  messages: [];
+  result: null;
+  success: false;
+  result_info: CloudflareResponseResultInfo;
+};
+
+type CloudflareBaseResponse<T> = CloudflareSuccessResponse<T> | CloudflareErrorResponse;
+
+type CloudflareVerifyTokenResponse = CloudflareBaseResponse<CloudflareVerifyToken>;
+
+type CloudflareListZonesResponse = CloudflareBaseResponse<CloudflareZone[]>;
+
+type CloudflareListEmailDestinationsResponse = CloudflareBaseResponse<CloudflareEmailDestination[]>;
+
+type CloudflareListEmailRulesResponse = CloudflareBaseResponse<CloudflareEmailRule[]>;
+
+export { CloudflareApiBaseUrl };
+export type {
+  CloudflareZone,
+  CloudflareListZonesResponse,
+  CloudflareVerifyToken,
+  CloudflareVerifyTokenResponse,
+  CloudflareEmailDestination,
+  CloudflareListEmailDestinationsResponse,
+  CloudflareEmailRule,
+  CloudflareListEmailRulesResponse,
+};
