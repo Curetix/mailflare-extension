3ef87143704f5f25ca88db7f5ec02d60bc70f88f
Curetix <me@curetix.eu>
2023-03-28 15:24:17 +0200

    Add vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "MailFlare | Email Alias",
+        short_name: "MailFlare",
+        description: "Create Email aliases for your own domains using Cloudflare Email Routing",
+        start_url: "/",
+        background_color: "#FFFFFF",
+        theme_color: "#FF922B",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
