74d4fa0195fcc3b24818eb4408d5ba60ace6d9c2
Curetix <me@curetix.eu>
2023-09-18 21:48:36 +0200

    Revert "Remove Worker"
    This reverts commit 7ac6332ba22aacfbaadcb4e828bcaf1d5df3467a.

---
+++ worker/email/services/mailjet.ts
@@ -0,0 +1,58 @@
+import fetcher from "shared/fetcher";
+import { Address, EmailService } from "./service.interface";
+
+type MailjetSendResponse = {
+  Messages: [
+    {
+      Status: string;
+      To: [
+        {
+          Email: string;
+          MessageUUID: string;
+          MessageID: number;
+          MessageHref: string;
+        },
+      ];
+    },
+  ];
+};
+
+export class Mailjet implements EmailService {
+  apiKey: string;
+  apiSecretKey: string;
+
+  constructor(apiKey: string, apiSecretKey: string) {
+    this.apiKey = apiKey;
+    this.apiSecretKey = apiSecretKey;
+  }
+
+  async send(from: Address, to: Address, subject: string, body: string) {
+    const payload = {
+      Messages: [
+        {
+          From: {
+            Email: from.email,
+            Name: from.name,
+          },
+          To: {
+            Email: to.email,
+            Name: to.name,
+          },
+          Subject: subject,
+          HTMLPart: body,
+        },
+      ],
+    };
+
+    const response = await fetcher<MailjetSendResponse>("https://api.mailjet.com/v3.1/send", {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${btoa(`${this.apiKey}:${this.apiSecretKey}`)}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    return response.Messages[0].Status === "success";
+  }
+}
