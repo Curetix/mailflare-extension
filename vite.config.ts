6b027baec1327b3d76c70f5883ee7b9bc2b94860
Curetix <me@curetix.eu>
2023-11-12 12:39:01 +0100

    Update icons

--- vite.config.ts
+++ vite.config.ts
@@ -22,23 +22,38 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      includeAssets: ["favicon.ico", "icons/192.png"],
       manifest: {
         name: "MailFlare - Email Aliases",
         short_name: "MailFlare",
         description: "Create Email aliases for your own domains using Cloudflare Email Routing",
+        lang: "en",
+        dir: "ltr",
         start_url: "/",
+        scope: "/",
+        display: "standalone",
+        prefer_related_applications: false,
         background_color: "#FFFFFF",
         theme_color: "#FF922B",
         icons: [
           {
-            src: "android-chrome-192x192.png",
-            sizes: "192x192",
+            src: "icons/128.png",
+            sizes: "128x128",
             type: "image/png",
           },
           {
-            src: "android-chrome-512x512.png",
+            src: "icons/256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icons/512.png",
             sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/1024.png",
+            sizes: "1024x1024",
             type: "image/png",
           },
         ],
