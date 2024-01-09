1aa4ea4595ee60a66cca8c2586cfbc7e50a808a2
Curetix <me@curetix.eu>
2024-01-09 17:58:28 +0100

    Init i18n

---
+++ src/i18n/en/index.ts
@@ -0,0 +1,7 @@
+import type { BaseTranslation } from "../i18n-types";
+
+const en = {
+  HI: "Hi {name:string}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n",
+} satisfies BaseTranslation;
+
+export default en;
