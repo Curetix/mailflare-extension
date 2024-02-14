7dd1503eeb364bb65a3812bc090bcb544312dcdb
Curetix <me@curetix.eu>
2024-02-14 17:49:52 +0100

    Remove webextension-polyfill

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,14 +2,13 @@ import type { LocaleDetector } from "typesafe-i18n/detectors";
 
 import { useEffect } from "react";
 import { useAtom } from "jotai";
-import browser from "webextension-polyfill";
 
 import App from "~app";
 import { popupHeight, popupWidth } from "~const";
 import { hostnameAtom } from "~utils/state";
 
 const detectBrowserLocale: LocaleDetector = () => {
-  return [browser.i18n.getUILanguage()];
+  return [chrome.i18n.getUILanguage()];
 };
 
 const detectors = [detectBrowserLocale];
@@ -19,7 +18,7 @@ export default function Popup() {
   const [, setHostname] = useAtom(hostnameAtom);
 
   useEffect(() => {
-    browser.tabs.query({ active: true }).then(([tab]) => {
+    chrome.tabs.query({ active: true }).then(([tab]) => {
       if (tab && tab.url) {
         const url = new URL(tab.url);
         setHostname(url.hostname);
